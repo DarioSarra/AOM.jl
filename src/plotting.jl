ecbaf2c2e77069f33dd2c63e904ab7f4edf0ec3a
DarioSarra <dariosarra@gmail.com>
2020-06-03 13:15:18 +0100

    update

--- src/plotting.jl
+++ src/plotting.jl
@@ -1,22 +1,22 @@
 function plot_results(lw::AbstractDataFrame)
     lw[!,:xname] = ["# $(Int64(round(r.rank_zscore_total))) $(r.name)" for r in eachrow(lw)]
     lw[!,:notey] = [(r.zscore_total >= 0.0 ? r.zscore_total + 0.5 : r.zscore_total - 0.5) for r in eachrow(lw)]
-    lw[!,:note] =[(r.rank_zscore_total, r.notey, Plots.text("$(round(r.zscore_total,digits=1))",:center,12)) for r in eachrow(lw)]
+    lw[!,:note] =[(r.rank_zscore_total, r.notey, Plots.text("$(round(r.zscore_total,digits=1))",:center,10)) for r in eachrow(lw)]
     sort!(lw,:rank_zscore_total)
     add_colors!(lw)
     fig = plot(ylabel = "Zscore",
         xlabel = "Ranking",
-        guidefontsize=18,
+        guidefont = (18, :darkgreen),
         bottom_margin = 100px,
         left_margin = 40px,
         size=(1000,1000))
     bar!(fig,lw.rank_zscore_total,lw.zscore_total,
         xticks = (1:nrow(lw),lw.xname),
-        xrotation=45,
+        tickfont = 12,
+        yticks = -20:1:20,
+        xrotation = 45,
         ylims = (minimum(lw.zscore_total)-1.2,maximum(lw.zscore_total)+1.2),
-        #fillcolor = lw.color,
         fillcolor = lw.heat,
-        tickfontsize = 13,
         legend = false)
     annotate!(lw.note)
 end
